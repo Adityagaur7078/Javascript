1bcfe751d8a034513e2d0a973eb5956f28c82030
Aditya Gaur <138069032+Adityagaur7078@users.noreply.github.com>
2025-04-04 17:41:47 +0000

    Variable File Added

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "adityagaur7078@gmail.com"
+var accountPassword = "12345"
+accountCity = "Kotdwar"
+let accountState;
+
+console.log(accountId);
+
+/*
+Prefer not ot use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
