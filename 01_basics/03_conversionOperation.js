a7a4f96f704afce0680abf55faced1fe5236a549
Aditya Gaur <138069032+Adityagaur7078@users.noreply.github.com>
2025-04-09 09:44:59 +0000

    Operations Complete

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "aditya"
 
-//console.log(typeof score);
-//console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-//console.log(typeof valueInNumber);
-//console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 // "33" => 33
@@ -15,7 +15,7 @@ let valueInNumber = Number(score)
 let isLoggedIn = 1
 
 let booleanIsLoggedIn =Boolean(isLoggedIn)
-//console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => flase; 
@@ -24,5 +24,45 @@ let booleanIsLoggedIn =Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(22%3);
+
+let str1 = "hello"
+let str2 = " cutie"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+// console.log(num1, num2, num3)
+
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// prefix Operation ++gameCounter;
+// postfix Operation gameCounter++;
