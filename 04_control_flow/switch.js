fecc08e5bb8a4a02cd4fe62961c60010017d76c8
Aditya Gaur <138069032+Adityagaur7078@users.noreply.github.com>
2025-05-08 13:10:42 +0000

    Control Flow switch topic Completed

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,77 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 2
+
+switch (month) {
+    case 1:
+        console.log("January")
+        break;
+    case 2:
+        console.log("February")
+        break;
+    case 3:
+        console.log("March")
+        break;
+    case 4:
+        console.log("April")
+        break;
+
+    default:
+        console.log("default case match")
+        break;
+}
+
+
+
+
+const Month = "Sep"
+
+switch (Month) {
+    case "Jan":
+        console.log("January")
+        break;
+    case "Feb":
+        console.log("February")
+        break;
+    case "Mar":
+        console.log("March")
+        break;
+    case "April":
+        console.log("April")
+        break;
+    case "May":
+        console.log("May")
+        break;
+    case "June":
+        console.log("June")
+        break;
+    case "July":
+        console.log("July")
+        break;
+    case "Aug":
+        console.log("August")
+        break;
+    case "Sep":
+        console.log("September")
+        break;
+    case "Oct":
+        console.log("October")
+        break;
+    case "Nov":
+        console.log("November")
+        break;
+    case "Dec":
+        console.log("December")
+        break;
+
+    default:
+        console.log("default case match")
+        break;
+}
